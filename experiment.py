0b760a5b75b24786492b82e04794a216eaa15bab
louashka <louashka@gmail.com>
2023-11-02 16:36:40 +0800

    Define the logic of the manual control through the keyboard in the class separate from the experiment file

--- experiment.py
+++ experiment.py
@@ -1,239 +1,81 @@
 import agent_controller
+import keyboard_controller
 import motive_client
 from pynput import keyboard
 import numpy as np
 import sys
 from nat_net_client import NatNetClient
 
-port_name = "COM5"
-controller = agent_controller.Controller(port_name)
+# Constants
+# PORT_NAME = "COM5"
 
-ROTATION_LEFT = {keyboard.KeyCode.from_char('a'), keyboard.Key.left}
-ROTATION_RIGHT = {keyboard.KeyCode.from_char('a'), keyboard.Key.right}
+OMNI_SPEED = 0.12
+ROTATION_SPEED = 1
+LU_SPEED = 0.12
 
-LEFT_LU_FORWARD = {keyboard.KeyCode.from_char('z'), keyboard.Key.up}
-LEFT_LU_BACKWARD = {keyboard.KeyCode.from_char('z'), keyboard.Key.down}
-RIGHT_LU_FORWARD = {keyboard.KeyCode.from_char('x'), keyboard.Key.up}
-RIGHT_LU_BACKWARD = {keyboard.KeyCode.from_char('x'), keyboard.Key.down}
-BOTH_LU_FORWARD = {keyboard.KeyCode.from_char('c'), keyboard.Key.up}
-BOTH_LU_BACKWARD = {keyboard.KeyCode.from_char('c'), keyboard.Key.down}
+# Create the controller
+# controller = agent_controller.Controller(PORT_NAME)
+kc = keyboard.Controller(OMNI_SPEED, ROTATION_SPEED, LU_SPEED)
 
-S1_SOFT = {keyboard.KeyCode.from_char(
-    'z'), keyboard.KeyCode.from_char('s'), keyboard.Key.up}
-S1_RIGID = {keyboard.KeyCode.from_char(
-    'z'), keyboard.KeyCode.from_char('s'), keyboard.Key.down}
-S2_SOFT = {keyboard.KeyCode.from_char(
-    'x'), keyboard.KeyCode.from_char('s'), keyboard.Key.up}
-S2_RIGID = {keyboard.KeyCode.from_char(
-    'x'), keyboard.KeyCode.from_char('s'), keyboard.Key.down}
-BOTH_SOFT = {keyboard.KeyCode.from_char('s'), keyboard.Key.up}
-BOTH_RIGID = {keyboard.KeyCode.from_char('s'), keyboard.Key.down}
-
-# The currently active modifiers
-current_keys = set()
-s = [0] * 2
 data = None
 agent_id = 1
 
+# Manual Control Function
 
-def manual_control(v, s, agent_id):
 
+def manualControl(v, s, agent_id):
     try:
-        w_current = motive_client.get_wheels_coords(data)
+        w_current = motive_client.getWheelsCoords(data)
 
         if w_current is not None:
-
-            omega = controller.get_wheels_velocities(w_current, v, s)
-            controller.move_robot(omega, s, agent_id)
-
-    except:
-        print("Error occured! The robot is stopped")
-        controller.move_robot(np.array([0, 0, 0, 0]), s, agent_id)
+            omega = controller.getWheelsVelocities(w_current, v, s)
+            controller.moveRobot(omega, s, agent_id)
+    except Exception as e:
+        print(f"Error occurred: {e}. The robot is stopped")
+        controller.moveRobot(np.array([0, 0, 0, 0]), s, agent_id)
 
 
-def on_press(key):
-    global current_keys, s
-
-    flag = True
-
-    v = [0] * 5
-
-    omni_speed = 0.12
-    rotation_speed = 1
-    lu_speed = 0.12
-
-    if key in ROTATION_LEFT:
-        current_keys.add(key)
-        if all(k in current_keys for k in ROTATION_LEFT):
-            print('Turn left')
-            flag = False
-            v[4] = rotation_speed
-
-    if key in ROTATION_RIGHT:
-        current_keys.add(key)
-        if all(k in current_keys for k in ROTATION_RIGHT):
-            print('Turn right')
-            flag = False
-            v[4] = -rotation_speed
-
-    if key in LEFT_LU_FORWARD:
-        current_keys.add(key)
-        if all(k in current_keys for k in LEFT_LU_FORWARD):
-            print("LU1 forward")
-            flag = False
-            v[0] = lu_speed
-
-    if key in LEFT_LU_BACKWARD:
-        current_keys.add(key)
-        if all(k in current_keys for k in LEFT_LU_BACKWARD):
-            print("LU1 backward")
-            flag = False
-            v[0] = -lu_speed
-
-    if key in RIGHT_LU_FORWARD:
-        current_keys.add(key)
-        if all(k in current_keys for k in RIGHT_LU_FORWARD):
-            print("LU2 forward")
-            flag = False
-            v[1] = lu_speed
-
-    if key in RIGHT_LU_BACKWARD:
-        current_keys.add(key)
-        if all(k in current_keys for k in RIGHT_LU_BACKWARD):
-            print("LU2 backward")
-            flag = False
-            v[1] = -lu_speed
-
-    if key in BOTH_LU_FORWARD:
-        current_keys.add(key)
-        if all(k in current_keys for k in BOTH_LU_FORWARD):
-            print("LUs forward")
-            flag = False
-            v[0] = lu_speed
-            v[1] = lu_speed
-
-    if key in BOTH_LU_BACKWARD:
-        current_keys.add(key)
-        if all(k in current_keys for k in BOTH_LU_BACKWARD):
-            print("LUs backward")
-            flag = False
-            v[0] = -lu_speed
-            v[1] = -lu_speed
-
-    if key in S1_SOFT:
-        current_keys.add(key)
-        if all(k in current_keys for k in S1_SOFT):
-            print("Segment 1 soft")
-            flag = False
-            s[0] = 1
-
-    if key in S1_RIGID:
-        current_keys.add(key)
-        if all(k in current_keys for k in S1_RIGID):
-            print("Segment 1 rigid")
-            flag = False
-            s[0] = 0
-
-    if key in S2_SOFT:
-        current_keys.add(key)
-        if all(k in current_keys for k in S2_SOFT):
-            print("Segment 2 soft")
-            flag = False
-            s[1] = 1
-
-    if key in S2_RIGID:
-        current_keys.add(key)
-        if all(k in current_keys for k in S2_RIGID):
-            print("Segment 2 rigid")
-            flag = False
-            s[1] = 0
-    if key in BOTH_SOFT:
-        current_keys.add(key)
-        if all(k in current_keys for k in BOTH_SOFT):
-            print("Both segments soft")
-            flag = False
-            s = [1, 1]
-
-    if key in BOTH_RIGID:
-        current_keys.add(key)
-        if all(k in current_keys for k in BOTH_RIGID):
-            print("Both segments rigid")
-            flag = False
-            s = [0, 0]
-
-    if flag:
-
-        if key == keyboard.Key.up:
-            print("Forward")
-            v[3] = omni_speed
-
-        if key == keyboard.Key.down:
-            print("Backward")
-            v[3] = -omni_speed
-
-        if key == keyboard.Key.left:
-            print("Left")
-            v[2] = -omni_speed
-
-        if key == keyboard.Key.right:
-            print("Right")
-            v[2] = omni_speed
-
-    manual_control(v, s, agent_id)
+def onPress(key):
+    v, s = kc.onPress()
+    manualControl(v, s, agent_id)
 
 
-def on_release(key):
+def onRelease(key):
     global current_keys
     try:
         current_keys.remove(key)
-        controller.move_robot(np.array([0, 0, 0, 0]), s, agent_id)
+        controller.moveRobot(np.array([0, 0, 0, 0]), s, agent_id)
     except KeyError:
         pass
 
 
-def receive_mocap_data_frame(mocap_data):
+def receiveMocapDataFrame(mocap_data):
     global data
-
     data = mocap_data
 
 
-def my_parse_args(arg_list, args_dict):
-    # set up base values
-    arg_list_len = len(arg_list)
-    if arg_list_len > 1:
-        args_dict["serverAddress"] = arg_list[1]
-        if arg_list_len > 2:
-            args_dict["clientAddress"] = arg_list[2]
-        if arg_list_len > 3:
-            if len(arg_list[3]):
-                args_dict["use_multicast"] = True
-                if arg_list[3][0].upper() == "U":
-                    args_dict["use_multicast"] = False
-
-    return args_dict
+def parseArgs(arg_list, args_dict):
+    # Implementation of parse_args method
+    pass
 
 
 if __name__ == "__main__":
+    # options_dict = {}
+    # options_dict["clientAddress"] = "127.0.0.1"
+    # options_dict["serverAddress"] = "127.0.0.1"
+    # options_dict["use_multicast"] = True
 
-    optionsDict = {}
-    optionsDict["clientAddress"] = "127.0.0.1"
-    optionsDict["serverAddress"] = "127.0.0.1"
-    optionsDict["use_multicast"] = True
+    # # This will create a new NatNet client
+    # options_dict = parseArgs(sys.argv, options_dict)
 
-    # This will create a new NatNet client
-    optionsDict = my_parse_args(sys.argv, optionsDict)
+    # streaming_client = NatNetClient()
+    # streaming_client.set_client_address(options_dict["clientAddress"])
+    # streaming_client.set_server_address(options_dict["serverAddress"])
+    # streaming_client.set_use_multicast(options_dict["use_multicast"])
 
-    streaming_client = NatNetClient()
-    streaming_client.set_client_address(optionsDict["clientAddress"])
-    streaming_client.set_server_address(optionsDict["serverAddress"])
-    streaming_client.set_use_multicast(optionsDict["use_multicast"])
+    # streaming_client.mocap_data_listener = receiveMocapDataFrame
 
-    # Configure the streaming client to call our markers handler on the emulator to send data out
-    streaming_client.mocap_data_listener = receive_mocap_data_frame
+    # is_running = streaming_client.run()
 
-    # Start up the streaming client now that the callbacks are set up.
-    # This will run perpetually, and operate on a separate thread.
-    is_running = streaming_client.run()
-
-    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    with keyboard.Listener(on_press=onPress, on_release=onRelease) as listener:
         listener.join()
---
+++ manual_motion.py
@@ -0,0 +1,141 @@
+from pynput import keyboard
+
+
+class KeyboardController:
+
+    def __init__(self, omni_speed, rotation_speed, lu_speed) -> None:
+
+        self.__omni_speed = omni_speed  # Speed of the agent in the rigid mode
+        # Speed of the agent's rotation in the rigid mode
+        self.__rotation_speed = rotation_speed
+        self.__lu_speed = lu_speed  # Speed of the locomotion units int thxe soft mode
+
+        self.__v = [0] * 5
+        self.__s = [0, 0]
+
+        self.__current_keys = set()
+
+        # Key combinations
+        self.__keyboard_combinations = {
+            'FORWARD': {keyboard.Key.up},
+            'BACKWARD': {keyboard.Key.down},
+            'RIGHT': {keyboard.Key.right},
+            'LEFT': {keyboard.Key.left},
+            'ROTATION_LEFT': {keyboard.KeyCode.from_char('w'), keyboard.Key.left},
+            'ROTATION_RIGHT': {keyboard.KeyCode.from_char('w'), keyboard.Key.right},
+            'HEAD_FORWARD': {keyboard.KeyCode.from_char('a'), keyboard.Key.up},
+            'HEAD_BACKWARD': {keyboard.KeyCode.from_char('a'), keyboard.Key.down},
+            'TAIL_FORWARD': {keyboard.KeyCode.from_char('d'), keyboard.Key.up},
+            'TAIL_BACKWARD': {keyboard.KeyCode.from_char('d'), keyboard.Key.down},
+            'HT_FORWARD': {keyboard.KeyCode.from_char('a'), keyboard.KeyCode.from_char('d'), keyboard.Key.up},
+            'HT_BACKWARD': {keyboard.KeyCode.from_char('a'), keyboard.KeyCode.from_char('d'), keyboard.Key.down},
+            'S1_SOFT': {keyboard.KeyCode.from_char('a'), keyboard.KeyCode.from_char('s'), keyboard.Key.up},
+            'S1_RIGID': {keyboard.KeyCode.from_char('a'), keyboard.KeyCode.from_char('s'), keyboard.Key.down},
+            'S2_SOFT': {keyboard.KeyCode.from_char('d'), keyboard.KeyCode.from_char('s'), keyboard.Key.up},
+            'S2_RIGID': {keyboard.KeyCode.from_char('d'), keyboard.KeyCode.from_char('s'), keyboard.Key.down},
+            'BOTH_SOFT': {keyboard.KeyCode.from_char('s'), keyboard.Key.up},
+            'BOTH_RIGID': {keyboard.KeyCode.from_char('s'), keyboard.Key.down}
+        }
+
+        self.__keyboard_actions = {
+            'FORWARD': {
+                'action': 'Agent forward',
+                'callback': lambda: self.__v.__setitem__(3, self.__omni_speed)
+            },
+            'BACKWARD': {
+                'action': 'Agent backward',
+                'callback': lambda: self.__v.__setitem__(3, -self.__omni_speed)
+            },
+            'RIGHT': {
+                'action': 'Agent right',
+                'callback': lambda: self.__v.__setitem__(2, self.__omni_speed)
+            },
+            'LEFT': {
+                'action': 'Agent left',
+                'callback': lambda: self.__v.__setitem__(2, -self.__omni_speed)
+            },
+            'ROTATION_RIGHT': {
+                'action': 'Turn agent to the right',
+                'callback': lambda: self.__v.__setitem__(4, -self.__rotation_speed)
+            },
+            'ROTATION_LEFT': {
+                'action': 'Turn agent to the left',
+                'callback': lambda: self.__v.__setitem__(4, self.__rotation_speed)
+            },
+            'HEAD_FORWARD': {
+                'action': 'Head LU forward',
+                'callback': lambda: self.__v.__setitem__(0, self.__lu_speed)
+            },
+            'HEAD_BACKWARD': {
+                'action': 'Head LU backward',
+                'callback': lambda: self.__v.__setitem__(0, -self.__lu_speed)
+            },
+            'TAIL_FORWARD': {
+                'action': 'Tail LU forward',
+                'callback': lambda: self.__v.__setitem__(1, self.__lu_speed)
+            },
+            'TAIL_BACKWARD': {
+                'action': 'Tail LU backward',
+                'callback': lambda: self.__v.__setitem__(1, -self.__lu_speed)
+            },
+            'HT_FORWARD': {
+                'action': 'Both LU\'s forward',
+                'callback': lambda: {self.__v.__setitem__(0, self.__lu_speed), self.__v.__setitem__(1, self.__lu_speed)}
+            },
+            'HT_BACKWARD': {
+                'action': 'Both LU\'s backward',
+                'callback': lambda: {self.__v.__setitem__(0, -self.__lu_speed), self.__v.__setitem__(1, -self.__lu_speed)}
+            },
+            'S1_SOFT': {
+                'action': 'Switch a head VSS to soft',
+                'callback': lambda: self.__s.__setitem__(0, 1)
+            },
+            'S1_RIGID': {
+                'action': 'Switch a head VSS to rigid',
+                'callback': lambda: self.__s.__setitem__(0, 0)
+            },
+            'S2_SOFT': {
+                'action': 'Switch a tail VSS to the soft mode',
+                'callback': lambda: self.__s.__setitem__(1, 1)
+            },
+            'S2_RIGID': {
+                'action': 'Switch a tail VSS to the rigid mode',
+                'callback': lambda: self.__s.__setitem__(1, 0)
+            },
+            'BOTH_SOFT': {
+                'action': 'Switch both VSS to the soft mode',
+                'callback': lambda: {self.__s.__setitem__(0, 1), self.__s.__setitem__(1, 1)}
+            },
+            'BOTH_RIGID': {
+                'action': 'Switch both VSS to the rigid mode',
+                'callback': lambda: {self.__s.__setitem__(0, 0), self.__s.__setitem__(1, 0)}
+            },
+        }
+
+    def __handleKeyCombination(self) -> list:
+        for combination, keys in self.__keyboard_combinations.items():
+            if (keys == self.__current_keys):
+                action = self.__keyboard_actions[combination]["callback"]
+                message = self.__keyboard_actions[combination]["action"]
+
+                print(message)
+                action()
+                break
+            else:
+                self.__v = [0] * 5
+                self.__s = [0, 0]
+
+        return self.__v, self.__s
+
+    def onPress(self, key) -> list:
+        self.__current_keys.add(key)
+
+        return self.__handleKeyCombination()
+
+    def onRelease(self, key) -> list:
+        try:
+            self.__current_keys.remove(key)
+        except KeyError:
+            self.__current_keys.clear()
+
+        return self.__handleKeyCombination()
