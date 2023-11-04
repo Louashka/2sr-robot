Fix the calculation of points rank along the curve

motive_client.py
@@ -58,17 +58,17 @@ def _unpack_data(mocap_data):
     return markers_df, rb_df
 
 
-def _motive_to_g(coords):
+def _motiveToG(coords):
     R_motive_to_g = np.array([[-1, 0, 0], [0, 0, 1], [0, 1, 0]])
 
     return np.matmul(R_motive_to_g, np.array(coords)).tolist()
 
 
-def _quat_tranform(args):
+def _quatTransform(args):
     return [-args[0], args[2], args[1], args[3]]
 
 
-def _euler_from_quaternion(coeffs):
+def _quaternionToEuler(coeffs):
     t0 = +2.0 * (coeffs[3] * coeffs[0] + coeffs[1] * coeffs[2])
     t1 = +1.0 - 2.0 * (coeffs[0] * coeffs[0] + coeffs[1] * coeffs[1])
     roll_x = np.arctan2(t0, t1)
@@ -85,53 +85,49 @@ def _euler_from_quaternion(coeffs):
     return [roll_x, pitch_y, yaw_z]  # in radians
 
 
-def _sort_points(markers_df, rb_df):
-    markers_df["order"] = 0
+def _rankPoints(markers_df, lu1_df):
 
-    lu1_df = rb_df[rb_df["id"] == 1]
-    free_markers_df = markers_df[markers_df["model_id"] == 0]
+    free_markers_df = markers_df[markers_df['model_id'] == 0]
 
-    vsf_start = free_markers_df.head(1)
-    min_dist = np.linalg.norm(
-        vsf_start[['marker_x', 'marker_y', 'marker_z']].values - lu1_df[['x', 'y', 'z']].values)
-    min_index = vsf_start.index[0]
+    # Define remaining points and their id's that correspond to thei original indicies
+    remaining_points = free_markers_df[[
+        'marker_x', 'marker_y', 'marker_z']].values.tolist()
+    remaining_points_id = list(range(0, 6))
 
-    for index, row in free_markers_df.iterrows():
-        dist = np.linalg.norm(
-            row[['marker_x', 'marker_y', 'marker_z']].values - lu1_df[['x', 'y', 'z']].values)
+    # List of points ranks
+    ranks = [0] * len(remaining_points)
+    rank = 1  # Current rank
 
-        if dist < min_dist:
-            min_dist = dist
-            min_index = index
+    # Find the point closest to the head LU
+    distances = np.linalg.norm(
+        remaining_points - lu1_df[['x', 'y', 'z']].values, axis=1)
 
-    markers_df.at[min_index, 'order'] = 1
-    free_markers_df.at[min_index, 'order'] = 1
+    current_index = distances.argmin()
+    current_point_id = remaining_points_id[current_index]
 
-    for i in range(1, 6):
-        current_df = free_markers_df[free_markers_df['order'] == 0]
+    # Find the rank of the rest of the points
+    while remaining_points:
+        current_point = remaining_points.pop(current_index)
+        current_point_id = remaining_points_id.pop(current_index)
 
-        current_point = free_markers_df[free_markers_df['order'] == i]
-        next_point = current_df.head(1)
+        ranks[current_point_id] = rank
+        rank += 1  # Update rank
 
-        min_dist = np.linalg.norm(next_point[['marker_x', 'marker_y', 'marker_z']
-                                             ].values - current_point[['marker_x', 'marker_y', 'marker_z']].values[0])
-        min_index = next_point.index[0]
+        if(remaining_points):
+            distances = np.linalg.norm(
+                remaining_points - np.array(current_point), axis=1)
+            current_index = distances.argmin()
 
-        for index, row in current_df.iterrows():
-            dist = np.linalg.norm(row[['marker_x', 'marker_y', 'marker_z']].values -
-                                  current_point[['marker_x', 'marker_y', 'marker_z']].values[0])
+    markers_df['order'] = 0
+    markers_df['order'][markers_df['model_id'] == 0] = ranks
 
-            if dist < min_dist:
-                min_dist = dist
-                min_index = index
+    # Sort the 'markers_df' DataFrame by order
+    sorted_markers_df = markers_df.sort_values(by=['order'])
 
-        markers_df.at[min_index, 'order'] = i + 1
-        free_markers_df.at[min_index, 'order'] = i + 1
-
-    return markers_df.sort_values(by=['order'])
+    return sorted_markers_df
 
 
-def _get_angle(p1, p2):
+def _getAngle(p1, p2):
 
     a = p2[0] - p1[0]
     b = p2[1] - p1[1]
@@ -141,7 +137,7 @@ def _get_angle(p1, p2):
     return alpha
 
 
-def __calc_wheels_coords(lu1_pos, lu1_angle, lu2_pos, lu2_angle):
+def __calcWheelsCoords(lu1_pos, lu1_angle, lu2_pos, lu2_angle):
     w1_0 = 2 * np.array([[-0.005], [-0.0325]])
     w2_0 = 2 * np.array([[0.0325], [0.0045]])
 
@@ -179,41 +175,56 @@ def __calc_wheels_coords(lu1_pos, lu1_angle, lu2_pos, lu2_angle):
     return w
 
 
-def get_wheels_coords(mocap_data):
-    w_coords = None
+# Calculate the wheels' coordinates from the mocap data
+def getWheelsCoords(mocap_data):
 
+    # A list of wheels coordinates
+    w = None
+
+    # Retreive location of the markers and rigid_bodies
     markers_df, rb_df = _unpack_data(mocap_data)
 
     if markers_df.empty or rb_df.empty:
         raise Exception("No data received from Motive!")
     else:
-        markers_df[['marker_x', 'marker_y', 'marker_z']] = markers_df[['marker_x', 'marker_y', 'marker_z']].apply(
-            lambda x: pd.Series([_motive_to_g(x)[0], _motive_to_g(x)[1], _motive_to_g(x)[2]]), axis=1)
+        # Convert values from the Motive frame to the global frame
+        markers_df[['marker_x', 'marker_y', 'marker_z']] = markers_df[[
+            'marker_x', 'marker_y', 'marker_z']].apply(lambda x: pd.Series(_motiveToG(x)), axis=1)
         rb_df[['x', 'y', 'z']] = rb_df[['x', 'y', 'z']].apply(lambda x: pd.Series(
-            [_motive_to_g(x)[0], _motive_to_g(x)[1], _motive_to_g(x)[2]]), axis=1)
-        rb_df[['a', 'b', 'c', 'd']] = rb_df[['a', 'b', 'c', 'd']].apply(lambda x: pd.Series(
-            [_quat_tranform(x)[0], _quat_tranform(x)[1], _quat_tranform(x)[2], _quat_tranform(x)[3]]), axis=1)
+            [_motiveToG(x)[0], _motiveToG(x)[1], _motiveToG(x)[2]]), axis=1)
+        rb_df[['a', 'b', 'c', 'd']] = rb_df[['a', 'b', 'c', 'd']].apply(
+            lambda x: pd.Series(_quatTransform(x)), axis=1)
 
-        rb_df[['roll', 'pitch', 'yaw']] = rb_df[['a', 'b', 'c', 'd']].apply(lambda x: pd.Series(
-            [_euler_from_quaternion(x)[0], _euler_from_quaternion(x)[1], _euler_from_quaternion(x)[2]]), axis=1)
-        markers_df = _sort_points(markers_df, rb_df)
+        # Convert quaternions to Euler angles
+        rb_df[['roll', 'pitch', 'yaw']] = rb_df[['a', 'b', 'c', 'd']].apply(
+            lambda x: pd.Series(_quaternionToEuler(x)), axis=1)
 
+        # Sort markers along the bridge
+        markers_df = _sortPoints(markers_df, rb_df)
+
+        # Get the position of the locomotion units
+        lu1_pos = [rb_df[rb_df["id"] == 1].x.values[0],
+                   rb_df[rb_df["id"] == 1].y.values[0]]
+
+        # Position of the tail LU is defined by the last marker in order
+        lu2_df = markers_df[(markers_df["model_id"] == 0)
+                            & (markers_df["order"] == 6)]
+        lu2_pos = [lu2_df.marker_x.values[0], lu2_df.marker_y.values[0]]
+
+        # Get the orientation of the head LU
         lu1_angle = rb_df[rb_df["id"] == 1].yaw.values[0]
 
+        # Get the position of the tail LU
         p = []
         for i in range(3, 6):
             p.append(markers_df[markers_df["order"] == i]
                      [["marker_x", "marker_y", "marker_z"]].values[0])
 
-        alpha1 = _get_angle(p[0], p[1])
-        alpha2 = _get_angle(p[1], p[2])
+        alpha1 = _getAngle(p[0], p[1])
+        alpha2 = _getAngle(p[1], p[2])
         lu2_angle = 2 * alpha2 - alpha1
 
-        lu1_pos = [rb_df[rb_df["id"] == 1].x.values[0], rb_df[rb_df["id"] == 1].y.values[0]]
+        # Calculate the wheels' coordinates from LU's coordinates
+        w = __calcWheelsCoords(lu1_pos, lu1_angle, lu2_pos, lu2_angle)
 
-        lu2_df = markers_df[(markers_df["model_id"] == 0) & (markers_df["order"] == 6)]
-        lu2_pos = [lu2_df.marker_x.values[0], lu2_df.marker_y.values[0]]
-
-        w_coords = __calc_wheels_coords(lu1_pos, lu1_angle, lu2_pos, lu2_angle)
-
-    return w_coords
+    return w